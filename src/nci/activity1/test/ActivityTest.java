e3942700b2f58f5b737f52d75827ac2465b8917f
Jennifer Casavantes <casavantesj@gmail.com>
2013-10-18 11:48:22 +0100

    Testing one step.

--- src/nci/activity1/test/ActivityTest.java
+++ src/nci/activity1/test/ActivityTest.java
@@ -14,7 +14,34 @@ public class ActivityTest {
 		assertTrue("Random generated out of bounds " + ran, ran > 0 && ran <= 80);
 		int ran2 = activity.generateRandom();
 		assertTrue("Random generated is equal to previous " + ran, ran2 != ran);
-		
+	}
+	
+	@Test
+	public void testReverseNumber() {
+		Activity1 activity = new Activity1();
+		int reverseNum = activity.generateReverseNum(32);
+		assertTrue("Random generated is not reverse " + reverseNum, reverseNum == 23);
+		reverseNum = activity.generateReverseNum(4);
+		assertTrue("Random generated is not reverse " + reverseNum, reverseNum == 4);
+		reverseNum = activity.generateReverseNum(10);
+		assertTrue("Random generated is not reverse " + reverseNum, reverseNum == 1);
 	}
 
+	@Test
+	public void testAdd() {
+		Activity1 activity = new Activity1();
+		int added = activity.add(32, 23);
+		assertTrue("Wrong addition " + added, added == 23 + 32);
+	}
+	
+	@Test
+	public void testResult() {
+		Activity1 activity = new Activity1();
+		int palin = activity.builPalindrome(5);
+		assertTrue("Is not palindrome " + palin, palin == 5);
+		palin = activity.builPalindrome(25);
+		assertTrue("Is not palindrome " + palin, palin == 25 + 52);
+	}
+	
+	
 }
--- src/nci/casavantesj/activity1/Activity1.java
+++ src/nci/casavantesj/activity1/Activity1.java
@@ -1,59 +1,60 @@
 package nci.casavantesj.activity1;
+
 import java.util.Random;
 
-
 public class Activity1 {
-	
-	
-	public Integer generateRandom(){
-		Random number =  new Random();
-		Integer mod = ((number.nextInt() % 80 ) + 1 );
-		
+
+	public Integer generateRandom() {
+		Random number = new Random();
+		Integer mod = ((number.nextInt() % 80) + 1);
+
 		return Math.abs(mod);
 	}
-	
-	public Integer generateReverseNum(int number){
+
+	public Integer generateReverseNum(int number) {
 		Integer var = number / 10;
 		Integer mod = number % 10;
-		
-		Integer reverseNum = (mod * 10) + var;
-		
-		return reverseNum;
-	}
-	
-	public boolean isPalindrome(Integer number, Integer reverseNum){
-		if (number.equals(reverseNum)){ 
-			return true;
+
+		if (number > 9) {
+			Integer reverseNum = (mod * 10) + var;
+			return reverseNum;
 		}
-		else return false;
-		
-		
+		return number;
 	}
-	
-	public int add(Integer number, Integer reverseNum){
+
+	public boolean isPalindrome(Integer number, Integer reverseNum) {
+		return number.equals(reverseNum);
+	}
+
+	public int add(Integer number, Integer reverseNum) {
 		return number + reverseNum;
-		
 	}
 	
-	
-	
+	public int builPalindrome(int val) {
+		int rev = generateReverseNum(val);
+		if (isPalindrome(val, rev))
+			return val;
+		return add(val,rev);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Activity1 act = new Activity1();
 		int number = act.generateRandom();
 		System.out.println(number);
-		//int reverse = act.generateReverseNum(number);
-		//act.isPalindrome(number, reverse);
-		//while(!act.isPalindrome(number, reverse)) {
-			//int result = act.add(number, reverse);
-			//System.out.println(number + reverse == result);
-			//number = reverse;
-			//reverse = result;
-		} 
-		//System.out.println(reverse);
-		
-		
+		// int reverse = act.generateReverseNum(number);
+		// act.isPalindrome(number, reverse);
+		// while(!act.isPalindrome(number, reverse)) {
+		// int result = act.add(number, reverse);
+		// System.out.println(number + reverse == result);
+		// number = reverse;
+		// reverse = result;
 	}
+	// System.out.println(reverse);
+	
+	
 
-//}
+
+}
+
+// }
